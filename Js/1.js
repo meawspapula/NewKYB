5db02df1e5748b40b573816e61147e510efc9bac
dreams... <meawspapula@gmail.com>
2025-05-28 14:59:02 +0300

    add Dev JS

---
+++ Js/1.js
@@ -0,0 +1,63 @@
+console.log("KYB");
+console.log(18);
+console.log("Welcome To the JS");
+
+const na="Car";
+const age=18;
+const stat="Student";
+ console.log(na+age+stat);
+
+ const height='12cm';
+ const mass='24kg'
+ const years='45'
+ console.log(height);
+ console.log(mass);
+ console.log(years);
+
+ const firstname="KY";
+ const lastname="B";
+ const fullname=firstname+""+lastname;
+ console.log(fullname);
+
+ console.log(15+12);
+ console.log(15*12);
+ console.log(15/12);
+ console.log(15-12);
+ console.log(15%12);
+
+ let num=10+2*5-(3+1);
+ console.log(num);
+
+let num2=10;
+num2+=2;
+console.log(num2);
+
+let num3=10;
+num3-=2;
+console.log(num3); 
+
+let num4=10;
+num4*=2;
+console.log(num4);
+
+let num5=10;
+num5/=2;
+console.log(num5);
+console.log(10/"abc");
+
+const name1="KYB";
+const age2="1month";
+console.log("Hello","My nmae is "+""+name1+""+"and I am "+""+age2+"years old.");
+
+const none= n;
+console.log(none);
+
+const none2=null;
+console.log(none2);
+
+const array=["apple","banana","pinaple","orange","kewi"];
+console.log(array);
+
+const isonline=true;
+console.log(isonline);
+
